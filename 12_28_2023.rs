1faaca580d1cc326897e7b6f6f2846d62744be10
Deepak Sankar <sdeepakishere@gmail.com>
2023-12-29 09:59:01 -0800

    Updating on 12_29_2023.rs
    Adding examples 8 and 9

--- 12_28_2023.rs
+++ 12_28_2023.rs
@@ -1,3 +1,5 @@
+use std::f64::consts;
+
 fn main() {
     // 1
     println!("Hello, world!");
@@ -35,6 +37,26 @@ fn main() {
     let val7 = 21;
     println!("Sum is {}", by_ref(&val7));
     println!("Sum of the value is {}", by_ref(&0));
+    
+    // 8
+    let x: f64 = 2.0 * consts::PI;
+    println!("Cosine Value is {}", x.cos());
+    
+    // 9 
+    let arr = [11, 22, 33, 44, 55];
+    for i in 0..arr.len() {
+        println!("Value at index {} = {}", i, arr[i]);
+    }
+    
+    // 10
+    let ints = [1, 2, 3];
+    let floats = [1.1, 2.1, 3.1];
+    let strings = ["hello", "world"];
+    let ints_ints = [[1, 2], [10, 20]];
+    println!("ints {:?}", ints);
+    println!("floats {:?}", floats);
+    println!("strings {:?}", strings);
+    println!("ints_ints {:?}", ints_ints);
 }
 
 // 5
@@ -49,6 +71,7 @@ fn factorial(n: u64) -> u64 {
     }
 }
 
+// 7
 fn by_ref(x: &i32) -> i32 {
     *x+1
 }
