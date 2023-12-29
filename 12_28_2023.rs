2c751a817ce1628c555a3884efc410623b7c0244
Deepak Sankar <sdeepakishere@gmail.com>
2023-12-29 09:33:11 -0800

    Update file on 12_29
    Adding examples 6 and 7.
    Understanding pass by reference

--- 12_28_2023.rs
+++ 12_28_2023.rs
@@ -27,7 +27,28 @@ fn main() {
     let res = sqr(2.0);
     println!("Square result is {}", res);
     
+    // 6
+    println!("Factorial of number is {}", factorial(5));
+    
+    // 7
+    // Pass by reference
+    let val7 = 21;
+    println!("Sum is {}", by_ref(&val7));
+    println!("Sum of the value is {}", by_ref(&0));
 }
 
 // 5
 fn sqr(x: f64)-> f64 {x*x}
+
+// 6
+fn factorial(n: u64) -> u64 {
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n-1)
+    }
+}
+
+fn by_ref(x: &i32) -> i32 {
+    *x+1
+}
